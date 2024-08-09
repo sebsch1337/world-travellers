fbaaaab74aedb29a878ca378403d00d2b36c2da1
Sebastian Scherbes <github@scherbes.de>
2024-08-09 14:06:55 +0200

    add project colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,16 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
+	theme: {
+		colors: {
+			azure: "#EDFDFA",
+			mint: "#DAFBF5",
+			celeste: "#B6F7EB",
+			turquoise: "#18DCB8",
+			night: "#04251F",
+		},
+	},
+	plugins: [],
 };
 export default config;
