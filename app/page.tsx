c36d5836bf92f1a7c9e9e3b68360b1df012d52e7
Sebastian Scherbes <github@scherbes.de>
2024-08-11 11:31:38 +0200

    create landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,34 @@
-import Image from "next/image";
+import { Button } from "@/components/button";
+import { Footer } from "@/components/footer";
+import { Header } from "@/components/header";
+import { IPhoneWrapper } from "@/components/iphone-wrapper";
+import { MainTitle, SubTitle, Title } from "@/components/title";
+
+import { MarketingSwiper } from "./_components/marketing-swiper";
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<p>Hello World</p>
-		</main>
+		<div className="h-dvh w-full sm:grid sm:place-items-center">
+			<IPhoneWrapper>
+				<div className="my-10 mx-4 h-full flex flex-col justify-between">
+					<Header isLandingPage />
+					<main className="flex-1 mx-4 flex flex-col justify-between relative">
+						<div>
+							<Title>
+								<MainTitle>tariff finder</MainTitle>
+								<SubTitle>Auslandskrankenversicherung</SubTitle>
+							</Title>
+
+							<div className="relative">
+								<MarketingSwiper />
+							</div>
+						</div>
+
+						<Button>tariff finder starten</Button>
+					</main>
+				</div>
+			</IPhoneWrapper>
+			<Footer />
+		</div>
 	);
 }
