bde27a1792c60db349b1689495d28d1966880e23
Sebastian Scherbes <github@scherbes.de>
2024-08-11 11:33:00 +0200

    create iphone wrapper component

---
+++ components/iphone-wrapper.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+import iPhone from "@/assets/iPhone.svg";
+
+interface IPhoneWrapperProps {
+	children: React.ReactNode;
+}
+
+export const IPhoneWrapper = ({ children }: IPhoneWrapperProps) => {
+	return (
+		<div className="h-full flex flex-col justify-between sm:w-[369.29px] sm:h-[770px] sm:aspect-[369.29/770] sm:rounded-[14.67%] relative bg-azure">
+			<Image
+				src={iPhone}
+				alt="iPhone 15 Pro"
+				className="absolute top-0 left-0 pointer-events-none hidden sm:block"
+			/>
+			{children}
+		</div>
+	);
+};
