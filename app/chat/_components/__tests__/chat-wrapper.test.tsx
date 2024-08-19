c20e34b30101675fcbbcdbecc9ea104e5fb8bf55
Sebastian Scherbes <github@scherbes.de>
2024-08-19 18:49:45 +0200

    create ChatWrapper tests

---
+++ app/chat/_components/__tests__/chat-wrapper.test.tsx
@@ -0,0 +1,61 @@
+import { render, screen } from "@testing-library/react";
+import { ChatWrapper } from "../chat-wrapper";
+
+jest.mock("react-markdown", () => {
+	const MockReactMarkdown = ({ children }: { children: React.ReactNode }) => {
+		return <div>{children}</div>;
+	};
+	MockReactMarkdown.displayName = "MockReactMarkdown";
+	return MockReactMarkdown;
+});
+
+jest.mock("openai", () => {
+	return {
+		__esModule: true,
+		default: jest.fn().mockImplementation(() => ({
+			beta: {
+				threads: {
+					runs: {
+						createAndPoll: jest.fn().mockResolvedValue({
+							id: "mocked-thread-id",
+							messages: [{ role: "assistant", content: "Mocked Assistant Response" }],
+						}),
+					},
+				},
+			},
+		})),
+	};
+});
+
+jest.mock("@/hooks/useChat", () => ({
+	useChat: jest.fn(() => ({
+		messages: [
+			{ id: "1", sender: "user", text: "Hello" },
+			{ id: "2", sender: "bot", text: "Hi there!" },
+		],
+		addMessage: jest.fn(),
+		sendMessage: jest.fn(),
+		isTyping: false,
+		setisTyping: jest.fn(),
+	})),
+}));
+
+jest.mock("@/hooks/useOpenAI", () => ({
+	useOpenAI: jest.fn(() => ({
+		isInitiated: true,
+		initThread: jest.fn(),
+		assistant: { id: "mocked-assistant-id" },
+		thread: { id: "mocked-thread-id" },
+	})),
+}));
+
+jest.mock("@/hooks/useScrollToBottom", () => ({
+	useScrollToBottom: jest.fn(() => null),
+}));
+
+describe("ChatWrapper Component", () => {
+	it("renders the ChatWrapper component", () => {
+		render(<ChatWrapper />);
+		expect(screen.getByPlaceholderText("Nachricht eingeben...")).toBeInTheDocument();
+	});
+});
