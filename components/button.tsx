213f34dd896be31f7cacf0baf72d3a8ced9c00ca
Sebastian Scherbes <github@scherbes.de>
2024-08-19 18:47:58 +0200

    create Button component

--- components/button.tsx
+++ components/button.tsx
@@ -1,15 +1,23 @@
 import { Montserrat_Alternates } from "next/font/google";
-const MontserratAlternates = Montserrat_Alternates({ subsets: ["latin"], weight: ["700"] });
+const MontserratAlternates = Montserrat_Alternates({ subsets: ["latin"], weight: ["600", "700"] });
 
 import { cn } from "@/utils/cn";
 
-interface ButtonProps {
-	children: React.ReactNode;
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+	className?: string;
 }
 
-export const Button = ({ children }: ButtonProps) => {
+export const Button = ({ onClick, className, children, ...props }: ButtonProps) => {
 	return (
-		<button className={cn(MontserratAlternates.className, "bg-turquoise w-full h-12 mb-4 text-lg select-none")}>
+		<button
+			onClick={onClick}
+			className={cn(
+				MontserratAlternates.className,
+				"bg-turquoise disabled:bg-turquoise/40 disabled:text-night/40 w-full h-12 mb-4 text-lg select-none",
+				className
+			)}
+			{...props}
+		>
 			{children}
 		</button>
 	);
