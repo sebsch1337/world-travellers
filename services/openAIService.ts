502f7e33a952c48cd8190a95333437e370806978
Sebastian Scherbes <github@scherbes.de>
2024-08-19 18:49:14 +0200

    create openAIService functions

---
+++ services/openAIService.ts
@@ -0,0 +1,37 @@
+"use server";
+
+import OpenAI from "openai";
+
+const openai = new OpenAI();
+
+import type { Assistant, Thread, ThreadDeleted } from "openai/resources/beta/index.mjs";
+
+const assistantId = process.env.OPEN_AI_ASSISTANT;
+
+export const createAssistant = async (): Promise<Assistant | null> => {
+	if (!assistantId) return null;
+	const newAssistant = await openai.beta.assistants.retrieve(assistantId);
+	return newAssistant;
+};
+
+export const createThread = async (): Promise<Thread> => await openai.beta.threads.create();
+
+export const deleteThread = async (thread: Thread): Promise<ThreadDeleted> => await openai.beta.threads.del(thread.id);
+
+export const sendAssistantMessage = async (assistant: Assistant, thread: Thread, newMessage: string): Promise<string> => {
+	const message = await openai.beta.threads.messages.create(thread.id, {
+		role: "user",
+		content: newMessage,
+	});
+
+	let run = await openai.beta.threads.runs.createAndPoll(thread.id, {
+		assistant_id: assistant.id,
+	});
+
+	if (run.status === "completed") {
+		const messages: any = await openai.beta.threads.messages.list(run.thread_id);
+		return messages.data[0].content[0].text.value;
+	} else {
+		throw new Error();
+	}
+};
