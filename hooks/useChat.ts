18a230ff330bb9972fbc832e29905245b7ef1536
Sebastian Scherbes <github@scherbes.de>
2024-08-25 19:45:59 +0200

    create appendToLastMessage function

--- hooks/useChat.ts
+++ hooks/useChat.ts
@@ -1,26 +1,27 @@
 import { toast } from "sonner";
 import { create } from "zustand";
 
-import { sendAssistantMessage } from "@/services/openAIService";
-
 import { Message } from "@/types/messageTypes";
 
-import type { Assistant } from "openai/resources/beta/assistants.mjs";
-import type { Thread } from "openai/resources/beta/index.mjs";
+import { AssistantStream } from "openai/lib/AssistantStream";
 
 interface ChatState {
 	messages: Message[];
 	isTyping: boolean;
+	inputDisabled: boolean;
 	addMessage: (text: string, sender: "user" | "bot", state?: "success" | "failed") => string;
 	changeMessageState: (id: string, newMessageState: "success" | "failed") => void;
-	sendMessage: (assistant: Assistant, thread: Thread, text: string) => Promise<void>;
-	setisTyping: (isTyping: boolean) => void;
+	sendMessage: (threadId: string, text: string) => Promise<void>;
+	appendToLastMessage: (text: string) => void;
 	clearMessages: () => void;
+	setIsTyping: (isTyping: boolean) => void;
+	setInputDisabled: (inputDisabled: boolean) => void;
 }
 
 export const useChat = create<ChatState>()((set, get) => ({
 	messages: [],
 	isTyping: false,
+	inputDisabled: false,
 
 	addMessage: (text, sender) => {
 		const newMessage: Message = {
@@ -43,30 +44,68 @@ export const useChat = create<ChatState>()((set, get) => ({
 		}));
 	},
 
-	sendMessage: async (assistant, thread, text) => {
-		const { addMessage, setisTyping, changeMessageState } = get();
+	appendToLastMessage: (text) => {
+		set((state) => {
+			const lastMessage = state.messages[state.messages.length - 1];
+			const updatedLastMessage = {
+				...lastMessage,
+				text: lastMessage.text + text,
+			};
+			return { messages: [...state.messages.slice(0, -1), updatedLastMessage] };
+		});
+	},
+
+	sendMessage: async (threadId, text) => {
+		const { addMessage, appendToLastMessage, setInputDisabled, setIsTyping, changeMessageState } = get();
 
 		const addedMessageId = addMessage(text, "user");
 
+		setInputDisabled(true);
+
 		setTimeout(async () => {
-			setisTyping(true);
+			setIsTyping(true);
 
 			try {
-				const responseText = await sendAssistantMessage(assistant, thread, text);
-				if (!responseText) throw new Error();
-				addMessage(responseText, "bot");
+				const response = await fetch(`/api/assistants/threads/${threadId}/messages`, {
+					method: "POST",
+					body: JSON.stringify({
+						content: text,
+					}),
+				});
+
+				if (!response.body) throw new Error();
+
+				const stream = AssistantStream.fromReadableStream(response.body);
+
+				stream.on("textCreated", () => {
+					setIsTyping(false);
+					addMessage("", "bot");
+				});
+
+				stream.on("textDelta", (delta) => {
+					if (delta.value != null) {
+						appendToLastMessage(delta.value);
+					}
+				});
+
+				stream.on("event", (event) => {
+					if (event.event === "thread.run.completed") {
+						setInputDisabled(false);
+					}
+				});
 			} catch (e) {
 				const error = e as Error;
 				if (error.message) console.error(error.message);
 				changeMessageState(addedMessageId, "failed");
+				setInputDisabled(false);
 				toast.error(`Nachricht konnte nicht gesendet werden. Bitte versuchen Sie es erneut.`);
-			} finally {
-				setisTyping(false);
 			}
 		}, 500);
 	},
 
-	setisTyping: (isTyping) => set({ isTyping }),
-
 	clearMessages: () => set({ messages: [] }),
+
+	setIsTyping: (isTyping) => set({ isTyping }),
+
+	setInputDisabled: (inputDisabled) => set({ inputDisabled }),
 }));
